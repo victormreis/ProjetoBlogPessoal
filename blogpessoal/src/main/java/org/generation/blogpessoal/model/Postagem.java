858a326ba95490cea77a05981d0aec26ad800a0a
Victor Martins <vitor_jhoul@hotmail.com>
2022-03-28 13:31:40 -0300

    Criando Tabela tema

--- blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -1,35 +1,42 @@
 package org.generation.blogpessoal.model;
+
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity // transforma em tabela (Criação no banco de dados)
-@Table(name ="tb_postagens") // nomeia a tabela
-public class Postagem {
-	
-	@Id // definir chave primaria
-	@GeneratedValue(strategy= GenerationType.IDENTITY) // autoincrement
-	private Long id;
-	
-	@NotNull // obrigatoriedade de campo
-	@Size (max= 100)
-	private String titulo;
-	
-	@Size(min=4,max=1000) // define o tamanho minimo e maximo do campo
-	private String texto;
-	
-	@UpdateTimestamp 
-	private LocalDateTime data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@Entity // transforma em tabela (Criação no banco de dados)
+@Table(name = "tb_postagens") // nomeia a tabela
+public class Postagem {
+
+	@Id // definir chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // autoincrement
+	private Long id;
+
+	@NotNull // obrigatoriedade de campo
+	@Size(max = 100)
+	private String titulo;
+
+	@Size(min = 4, max = 1000) // define o tamanho minimo e maximo do campo
+	private String texto;
+
+	@UpdateTimestamp
+	private LocalDateTime data;
 	
-	// Metodos Getters and Setters 
-	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	// Metodos Getters and Setters
+
 	public Long getId() {
 		return id;
 	}
@@ -60,9 +67,14 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
-	} 
-	
-	
-	
-	
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
